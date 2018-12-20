6caeba94440826ebf6c93ceb4c34fe191c38691a
kingbri <bdashore3@gmail.com>
2018-12-20 16:31:10 -0500

    Add sqlite importing
    "Removed the placeholder from system and added ianmacd's sqlite binary
    to avoid the possibilty of people installing 2 modules"
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- common/install.sh
+++ common/install.sh
@@ -2,7 +2,21 @@
 
 #Install script for BlackenedMod
 
-#Copy zipalign script
+#Install sqlite binary and copy zipalign script
+ui_print " "; ui_print "Installing sqlite binary by ianmacd"
+local bindir=/system/bin
+local xbindir=/system/xbin
+
+# Check for existence of /system/xbin directory.
+if [ ! -d /sbin/.core/mirror$xbindir ]; then
+	# Use /system/bin instead of /system/xbin.
+	mkdir -p $MODPATH$bindir
+    	mv $MODPATH$xbindir/sqlite3 $MODPATH$bindir
+	rmdir $MODPATH$xbindir
+	xbindir=$bindir
+fi;
+ui_print "- Installed sqlite to $xbindir"
+
 cp -af $INSTALLER/common/Zipalign_sqlite.sh /data/adb/service.d
 chmod 0755 /data/adb/service.d/Zipalign_sqlite.sh
 
