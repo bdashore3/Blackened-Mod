ac8e31531eb4b7b220443974c8c1069448976473
kingbri <bdashore3@gmail.com>
2018-12-20 17:34:10 -0500

    Fix my own stupidity
    "Just..."
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- common/install.sh
+++ common/install.sh
@@ -22,14 +22,14 @@ chmod 0755 /data/adb/service.d/Zipalign_sqlite.sh
 
 #set device variable
 device="$(getprop ro.product.device)"
-if [ $device == "marlin" ] || [ $device == "sailfish"] ; then
+if [ $device == "marlin" ] || [ $device == "sailfish" ] ; then
 	ui_print " "; ui_print "You are using Pixel(XL), removing wahoo files"
 	cp -af $INSTALLER/common/marlin/02BlackenedMod.sh /data/adb/service.d
 	chmod 0755 /data/adb/service.d/02BlackenedMod.sh
 	rm -r $INSTALLER/common/marlin
 	rm -r $INSTALLER/common/wahoo
 	ui_print "Done"
-elif [ $device == "walleye" ] || [ $device == "taimen"] ; then
+elif [ $device == "walleye" ] || [ $device == "taimen" ] ; then
 	ui_print "You are using Pixel 2(XL), removing OG pixel files"
 	cp -af $INSTALLER/common/wahoo/02BlackenedMod.sh /data/adb/service.d
 	chmod 0755 /data/adb/service.d/02BlackenedMod.sh
