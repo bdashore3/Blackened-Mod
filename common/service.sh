87f67f7586f88957875e4522c0651c2efdba2787
bdashore3 <bdashore3@gmail.com>
2019-01-21 18:33:05 -0500

    Fixup bluecross naming scheme
    "Mod wasn't named properly, so it didn't install"
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- common/service.sh
+++ common/service.sh
@@ -1,5 +1,6 @@
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
-sh '02BlackenedMod.sh'
-sh '03KingKernel.sh'
-sh 'Zipalign_sqlite.sh'
+
+sh '/sbin/.magisk/img/BlackenedMod/02BlackenedMod.sh'
+sh '/sbin/.magisk/img/BlackenedMod/03KingKernel.sh'
+sh '/sbin/.magisk/img/BlackenedMod/Zipalign_sqlite.sh'
